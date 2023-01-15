67a0878442860b6ae9610170a3b70827d0bc61c4
GreeninZ <lukasz.staporek15@gmail.com>
2023-01-15 21:20:51 +0100

    usuniecie unity preojektu

--- pythontest/Assets/glowny.py
+++
@@ -1,9 +0,0 @@
-import UnityEngine as unity
-from jeden import zmienna
-
-if zmienna == "elo":
-    tekst = unity.GameObject.Find("wynik")
-    tekst.text = "dobrze"
-else:
-    tekst = unity.GameObject.Find("wynik")
-    tekst.text = "zle"
