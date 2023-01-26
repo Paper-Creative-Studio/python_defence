769c4c5ff68eadf84fbcc41ba8d7b7fe606ebecc
GreeninZ <lukasz.staporek15@gmail.com>
2023-01-26 13:33:45 +0100

    Implementacja animacji chodzenia w góre, kamera sledzi gracza

--- Python_Defense/Assets/Skrypty/Mark/movement.cs
+++ Python_Defense/Assets/Skrypty/Mark/movement.cs
@@ -10,6 +10,7 @@ public class movement : MonoBehaviour
     private Vector2 smoothmove;
     private Vector2 smoothInputVelocity;
     [SerializeField] private float smoothing_speed;
+    [SerializeField] private Animator anim_controller;
     private Vector2 direction;
     // Start is called before the first frame update
     void Start()
@@ -22,14 +23,31 @@ public class movement : MonoBehaviour
     {
         input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
         smoothmove = Vector2.SmoothDamp(smoothmove, input, ref smoothInputVelocity, smoothing_speed);
+
+        if (Input.GetAxisRaw("Horizontal") != 0 && Input.GetAxisRaw("Vertical") <= 0)
+        {
+            anim_controller.SetBool("MovingLeftRight", true);
+            anim_controller.SetBool("MovingUp", false);
+        }
+        else if(Input.GetAxisRaw("Vertical") > 0)
+        {
+            anim_controller.SetBool("MovingLeftRight", false);
+            anim_controller.SetBool("MovingUp", true);
+        }
+        else if(Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
+        {
+            anim_controller.SetBool("MovingLeftRight", false);
+            anim_controller.SetBool("MovingUp", false);
+        }
         
-        
-        
+
+
+
     }
     private void FixedUpdate()
     {
         rb.velocity = smoothmove * speed;
-        Debug.Log(rb.velocity);
+        Debug.Log(anim_controller.GetBool("MovingLeftRight") + " " + anim_controller.GetBool("MovingUp"));
     }
 
 }
