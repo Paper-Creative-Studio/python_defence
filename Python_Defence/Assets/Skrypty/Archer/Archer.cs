7c497021523096c39ffdaf687f1a9e930243975c
GreeninZ <lukasz.staporek15@gmail.com>
2023-02-07 21:01:33 +0100

    łucznik umie celowac - opis
    do zrobienia rotacja strzal

--- Python_Defence/Assets/Skrypty/Archer/Archer.cs
+++ Python_Defence/Assets/Skrypty/Archer/Archer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -15,11 +16,25 @@ public class Archer : MonoBehaviour
     public Animator anim_controller;
     
     public LayerMask enemyLayers;
-    [Range(0, 360)]
-    public float FOVAngle;
+   
     
     public float attackRange;
 
+    private Vector2 gizmosPosition;
+
+    public Transform controlPoint;
+
+    
+    private float CurveTime = 0f;
+
+    private float arrowSpeed = 0.5f;
+    private bool coroutineAllowed = true;
+    private GameObject createdArrow;
+    private Vector3 arrowPos;
+    private float arrowY2;
+    Vector2 diff;
+    bool asad;
+    Vector3 enemy;
     // Start is called before the first frame update
     void Start()
     {
@@ -31,36 +46,74 @@ public class Archer : MonoBehaviour
     {
         if (canAttack)
         {
-            StartCoroutine(FindTarget());
+            
             canAttack = false;
             anim_controller.SetTrigger("Shoot");
             StartCoroutine(Cooldown());
            
         }
-        
+
+        Debug.Log(createdArrow.transform.rotation);
     }
     public void shootArrow()
     {
-        Instantiate(arrow, shootPoint.position, Quaternion.identity);
-       
+
+        createdArrow = (GameObject)Instantiate(arrow, shootPoint.position, Quaternion.identity);
+        enemy = target.position;
+        if (coroutineAllowed)
+        {
+            StartCoroutine(ArrowMove());
+        }
+        
+        CurveTime = 0f;
+
+
+    }
+    IEnumerator ArrowMove()
+    {
+        
+        coroutineAllowed = false;
+        while (CurveTime < 1)
+        {
+            //Movement strzaly
+            arrowPos = createdArrow.transform.position;
+            CurveTime += Time.deltaTime * arrowSpeed;
+            createdArrow.transform.position = Mathf.Pow(1 - CurveTime, 2) * transform.position + 2 * (1 - CurveTime) * CurveTime * controlPoint.position + Mathf.Pow(CurveTime, 2) * enemy;
+            
+            if (createdArrow.transform.position.y - arrowPos.y >= 0)
+            {
+                arrowSpeed = 0.5f;
+            }
+            else
+            {
+                
+                arrowSpeed -= -0.80f * Time.deltaTime; //default gravity to -9.14
+            }
+
+            //Rotacja strzaly
+            diff = arrowPos - createdArrow.transform.position;
+            createdArrow.transform.eulerAngles = new Vector3(69,69,69);
+
+            yield return new WaitForEndOfFrame();
+        }
+        CurveTime = 0f;
+        coroutineAllowed = true;
     }
     IEnumerator Cooldown()
     {
         yield return new WaitForSeconds(5);
         canAttack = true;
     }
-    IEnumerator FindTarget()
+
+    private void OnDrawGizmos()
     {
-        yield return new WaitForSeconds(0.2f);
-        Collider2D[] hitEnemy = Physics2D.OverlapCircleAll(transform.position, attackRange, enemyLayers);
-        if (hitEnemy.Length != 0)
+        for(float t =0;t<=1;t+=0.05f)
         {
-            target = hitEnemy[0].transform;
-            Vector2 directionToTarget = target.position - transform.position;
-            if (Vector2.Angle(transform.forward, directionToTarget) < FOVAngle / 2)
-            {
-                Debug.Log("Target: " + target);
-            }
+            gizmosPosition = Mathf.Pow(1 - t, 2) * transform.position + 2 * (1 - t)  * t * controlPoint.position + Mathf.Pow(t,2) * target.position;
+
+            Gizmos.DrawSphere(gizmosPosition, 0.25f);
         }
+        Gizmos.DrawLine(new Vector3(transform.position.x, transform.position.y, transform.position.z), new Vector3(controlPoint.position.x, controlPoint.position.y, controlPoint.position.z));
+        Gizmos.DrawLine(new Vector3(controlPoint.position.x, controlPoint.position.y, controlPoint.position.z), new Vector3(target.position.x, target.position.y, target.position.z));
     }
 }
--- Python_Defence/Assets/Skrypty/arrow.cs
+++ Python_Defence/Assets/Skrypty/arrow.cs
@@ -1,22 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.U2D.Path;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class arrow : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
+    
+    
+    
+    public Transform controlPoint;
     // Start is called before the first frame update
     void Start()
     {
-        rb.velocity = new Vector2(-4, 4);
+        rb = GetComponent<Rigidbody2D>();
     }
     
     // Update is called once per frame
     void Update()
     {
         float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle - 125f, Vector3.forward) ;
+        transform.rotation = Quaternion.AngleAxis(angle - 125f, Vector3.forward);
         Destroy(gameObject, 3);
-        
+
     }
+   
+    
 }
