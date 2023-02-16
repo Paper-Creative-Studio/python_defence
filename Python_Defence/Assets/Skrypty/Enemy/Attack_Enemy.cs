8cdb476452adbdb4d497ef632d5d14c53dba0e44
GreeninZ <lukasz.staporek15@gmail.com>
2023-02-16 17:15:19 +0100

    Skonczony szkielet

--- Python_Defence/Assets/Skrypty/Enemy/Attack_Enemy.cs
+++ Python_Defence/Assets/Skrypty/Enemy/Attack_Enemy.cs
@@ -3,7 +3,9 @@ using Pathfinding;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.U2D.Path;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Attack_Enemy : MonoBehaviour
 {
@@ -19,12 +21,19 @@ public class Attack_Enemy : MonoBehaviour
     public Animator anim;
    
     public Collider2D[] hitPlayer;
-    
+    public GameObject arrow;
     public GameObject bullet;
     public float speed;
     private GameObject spell;
-    
-    public Vector3 spelltarget;
+    private GameObject createdArrow;
+    public Vector3 target;
+    private float CurveTime = 0f;
+    private bool coroutineAllowed = true;
+    private Vector3 arrowNextPos;
+    public Transform controlPoint;
+    private Vector3 gizmosPosition;
+    public bool printuj = false;
+    private float arrowspeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -70,22 +79,105 @@ public class Attack_Enemy : MonoBehaviour
     public void MageAttack()
     {
         spell = (GameObject)Instantiate(bullet, attackPoint.position, Quaternion.identity);
-        spelltarget = hitPlayer[0].transform.position;
-        spell.GetComponent<spell>().target= spelltarget;
+        target = hitPlayer[0].transform.position;
+        spell.GetComponent<spell>().target= target;
         
        
         
     }
-        
+    public void shootArrow()
+    {
+
+        createdArrow = (GameObject)Instantiate(arrow, attackPoint.position, Quaternion.identity);
+        target = hitPlayer[0].transform.position;
+        if (coroutineAllowed)
+        {
+            StartCoroutine(ArrowMove());
+        }
+        CurveTime = 0f;
+
+
+    }
+    IEnumerator ArrowMove()
+    {
+        arrowNextPos = createdArrow.transform.position;
+
+        coroutineAllowed = false;
+
+
+        while (CurveTime < 1)
+        {
+            //Movement strzaly
+            if (createdArrow != null)
+            {
+                if ( target.x - transform.position.x <= 4.5f)
+                {
+                    arrowspeed = 0.85f;
+                    CurveTime += Time.deltaTime * arrowspeed;
+                    createdArrow.transform.position = Vector3.MoveTowards(createdArrow.transform.position, target, arrowspeed * Time.fixedDeltaTime);
+                    Vector3 dir = target - createdArrow.transform.position;
+                    var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                    createdArrow.transform.rotation = Quaternion.AngleAxis(angle - 45f, Vector3.forward);
+                    
+                }
+                else
+                {
+                    CurveTime += Time.deltaTime * arrowspeed;
+
+                    createdArrow.transform.position = Vector3.MoveTowards(createdArrow.transform.position, arrowNextPos, arrowspeed);
+                    arrowNextPos = Mathf.Pow(1 - CurveTime, 2) * attackPoint.position + 2 * (1 - CurveTime) * CurveTime * controlPoint.position + Mathf.Pow(CurveTime, 2) * target;
+
+                    //Rotacja strzaly
+
+                    Vector3 dir = arrowNextPos - createdArrow.transform.position;
+                    var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                    createdArrow.transform.rotation = Quaternion.AngleAxis(angle - 45f, Vector3.forward);
+                    if (createdArrow.transform.position.y - arrowNextPos.y <= 0)
+                    {
+                        arrowspeed = 0.7f;
+                    }
+                    else
+                    {
+
+                        arrowspeed -= -0.87f * Time.deltaTime; //default gravity to -9.14
+                    }
+                }
+                
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+
+
+        CurveTime = 0f;
+        coroutineAllowed = true;
+    }
+
     IEnumerator Cooldown()
     {
         yield return new WaitForSeconds(attackcooldown);
         canAttack = true;
     }
-    
+
     //private void OnDrawGizmosSelected()
     //{
     //    Gizmos.DrawSphere(attackPoint.position, attackRange);
     //}
+    private void OnDrawGizmos()
+    {
+        //if(printuj)
+        //{
+        //    for (float t = 0; t <= 1; t += 0.05f)
+        //    {
+        //        gizmosPosition = Mathf.Pow(1 - t, 2) * attackPoint.position + 2 * (1 - t) * t * controlPoint.position + Mathf.Pow(t, 2) * target;
+
+        //        Gizmos.DrawSphere(gizmosPosition, 0.25f);
+        //    }
+        //    Gizmos.DrawLine(new Vector3(attackPoint.position.x, attackPoint.position.y, attackPoint.position.z), new Vector3(controlPoint.position.x, controlPoint.position.y, controlPoint.position.z));
+
+        //    Gizmos.DrawLine(new Vector3(controlPoint.position.x, controlPoint.position.y, controlPoint.position.z), new Vector3(target.x, target.y, target.z));
+        //}
+    }
+        
 
 }
--- Python_Defence/Assets/enemyarrow.cs
+++ Python_Defence/Assets/enemyarrow.cs
@@ -1,37 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using UnityEditor.U2D.Path;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
-public class arrow : MonoBehaviour
+public class enemyarrow : MonoBehaviour
 {
     public int damage;
- 
+
     // Start is called before the first frame update
     void Start()
     {
-       
+
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-      
+
         Destroy(gameObject, 3);
 
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
-        if(collision.gameObject.layer == 6)
+
+        if (collision.gameObject.layer == 8)
         {
-            collision.gameObject.GetComponent<Enemy_Health>().TakeDamage(damage);
+            collision.gameObject.GetComponent<Health>().TakeDamage(damage);
         }
+        Debug.Log(collision.gameObject.layer);
         Destroy(gameObject);
 
 
     }
-
 }
