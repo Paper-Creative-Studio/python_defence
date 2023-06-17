b29c68702d2b82da0145f77f64bb009f489d2c77
GreeninZ <lukasz.staporek15@gmail.com>
2023-06-17 13:03:39 +0200

    build changes

--- Builds/Python defence  0.2.5 SCI++ Final/Python Defence_Data/pyton.py
+++
@@ -1,8 +0,0 @@
-exec()
-a=5
-b=6
-wynik = addition(a, b)
-if a + b == addition(a, b):
-	print("Returned value is addition result of two arguments")
-else:
-	print("Returned value is not addition result of two arguments")
